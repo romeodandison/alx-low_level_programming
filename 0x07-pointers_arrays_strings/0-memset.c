a54b76a3b615570393ca941b5c2ee5919ed38e50
romeodandison <dandisonromeo@gmail.com>
2022-11-02 08:53:13 -0700

    memstat

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,11 +7,11 @@
  * @n: the numeber of bytes to be filled
  * Return: change array value of n bytes
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (n > 0; i++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
